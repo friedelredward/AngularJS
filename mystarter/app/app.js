e1e452ffa66dc76f950d38a7d192b972f37ed2c0
friedelredward <RobertEdward.Friedel@hpecds.com>
2020-11-04 18:44:05 +0100

    Add: Hello world from mystarter JS

--- mystarter/app/app.js
+++ mystarter/app/app.js
@@ -1,3 +1,7 @@
 'use strict';
 
 angular.module('myApp', []);//se declara vacio y se va rellenando
+angular.module('myApp')
+            .controller('MainController', ['$scope', function(){
+                console.log("Hello ANgUlAR JS")
+            }]);
