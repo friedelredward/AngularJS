c0a5667fe0ef0e08cf6c409bdcb2c1717f2528c1
friedelredward <RobertEdward.Friedel@hpecds.com>
2020-11-05 13:30:28 +0100

    Add: finished Introduction: filters, 2way databinding controlersas alias +this, services, scope and basic_injection

--- mystarter/app/app.js
+++ mystarter/app/app.js
@@ -25,7 +25,74 @@ angular.module('myApp')
                 $scope.itemIndex= Math.round(Math.random() * (frutas.length - 1));
                 $scope.setItem();
             }, 1000);
-
-
+}]);
+/**Scope y model */
+angular.module('myApp')
+        .controller('FatherController', ['$scope', function($scope){
+            console.log("Hello from father");
+            
+            $scope.mimensaje=" Hellow";
+            $scope.model={
+                mimensaje:" modelHellow"
+            };
 
 }]);
+angular.module('myApp')
+        .controller('ChildController', ['$scope', function($scope){
+            console.log("Hello Hello from son");
+
+}]);
+
+/**databinding with service */
+angular.module('myApp').service('SharedService', function(){
+    return { mimensaje: 'injectado con service'};
+});
+
+angular.module('myApp')
+        .controller('indieController', ['$scope','SharedService', function($scope, SharedService){
+            console.log(" Hello from indie1");
+
+            $scope.model= SharedService;
+}]);
+angular.module('myApp')
+        .controller('indieController2', ['$scope','SharedService', function($scope, SharedService){
+            console.log("Hello Hello from indie2");
+            $scope.model= SharedService;
+}]);
+/**as alias and father syntax */
+angular.module('myApp')
+        .controller('sintaxFatherController', [ function(){
+
+         this.mimensaje="mensaje desde Fathercontroller con THIS syntax /no scope"   
+}]);
+/**para interval y watche we need the scope injected */
+angular.module('myApp')
+        .controller('sintaxController', ['$scope','$interval', function($scope,$interval){
+
+            this.mimensaje="mensaje desde controller con THIS syntax /no scope"
+            this.valor=0;
+
+            /**cada x secs valor= nro random */
+            $interval(function(){
+             this.valor= Math.round(Math.random()*100000)+1;
+            }.bind(this), 5000);
+            /**watcher que loguea mi valor */
+            $scope.$watch('sintaxC.valor', function(newValue, oldValue){
+                console.log("NewV:"+newValue+" || OldVal= "+oldValue);
+            });
+}]);
+/**filters pej capitalize */
+angular.module('myApp').filter('capitalize', function(){
+    return function(value){
+        var result =null;
+        var words = value.split(' ');
+        words.forEach(element => {
+            if(result){
+                result+=' ';
+            }else{
+                result ='';
+            }
+           result += element.substr(0,1).toUpperCase() + element.substr(1).toLowerCase();
+        }); return result;
+    }
+});
