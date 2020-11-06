5aa7f62eee2a7bcf72e5324b1c66743bffa32732
friedelredward <RobertEdward.Friedel@hpecds.com>
2020-11-06 12:47:23 +0100

    ADD: project root dir fix + finished begginer lvl; prepared for intermediate

---
+++ intermediate/isolatescopes/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp', []);
+
+angular.module('myApp')
+        .controller('MainController', ['$scope','$interval', function($scope, $interval){
+            
+            var vm=this;
+
+}]);
+
+/**as alias and father syntax */
+angular.module('myApp')
+        .controller('exampleController', [ function(){
+            var vm=this;  
+}]);
